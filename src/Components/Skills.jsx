1eb5c011bc9186d0a1d0aa673596052e81aa30f2
Sa3fu <saifuden786@gmail.com>
2024-03-19 17:10:21 +0530

    added development skills part and design part

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,18 +1,49 @@
 import React from "react";
 import settingsImg from "../assets/settingsImg.png";
+import { design, webDevelopment } from "../Components/index";
+import webDev from "../assets/webdev.png";
 
 function Skills() {
   return (
     <div className="bg-stone-900 p-5">
-      <h1 className="text-4xl text-white font-medium mb-5 font-titleFont">Skills</h1>
+      <h1 className="text-4xl text-white font-medium mb-5 font-titleFont">
+        Skills
+      </h1>
       <div className="border-[1px] border-cyan-400 w-36 mb-10"></div>
-      <div>
-        <h2 className="flex items-center gap-3">
+      
+      <div className="mb-6">
+        <h2 className="flex items-center gap-3 mb-4">
           <span>
             <img src={settingsImg} alt="" className="w-9" />
           </span>
           <span className="text-white text-xl font-medium">DEVELOPMENT</span>
         </h2>
+        <ul className="text-white grid grid-cols-3 gap-2 items-center text-xs">
+          {webDevelopment.map((item) => (
+            <li
+              key={item._id}
+              className="border-[.5px] rounded-full p-5 flex items-center justify-center w-20 h-20 border-slate-600"
+            >
+              {item.text}
+            </li>
+          ))}
+        </ul>
+      </div>
+      
+      <div className="mb-6">
+        <h2 className="flex items-center gap-3 mb-5">
+          <span>
+            <img src={webDev} alt="" className="w-9" />
+          </span>
+          <span className="text-white text-xl font-medium">DESIGN</span>
+        </h2>
+        <ul className="grid grid-cols-3 gap-3 w-52">
+          {design.map((item) => (
+            <li key={item._id} className="w-10">
+              <img src={item.img} alt={item.text} />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
---
+++ src/Components/index.js
@@ -0,0 +1,62 @@
+import figma from "../assets/figma.256x256.png";
+import adobeAE from "../assets/adobe-after-effects.256x256.png";
+import adobeAi from "../assets/adobe-illustrator.256x256.png";
+import adobePS from "../assets/adobe-photoshop.256x256.png";
+import canva from "../assets/canva.png";
+import adobeIn from "../assets/indesign.256x250.png";
+
+export const webDevelopment = [
+  {
+    _id: "1",
+    text: "Javascript",
+  },
+  {
+    _id: "2",
+    text: "HTML5",
+  },
+  {
+    _id: "3",
+    text: "CSS3",
+  },
+  {
+    _id: "4",
+    text: "ReactJs",
+  },
+  {
+    _id: "5",
+    text: "Tailwind CSS",
+  },
+  {
+    _id: "6",
+    text: "Node.js",
+  },
+  {
+    _id: "7",
+    text: "MongooDB",
+  },
+  {
+    _id: "8",
+    text: "Python",
+  },
+  {
+    _id: "9",
+    text: "AngularJs",
+  },
+  {
+    _id: "10",
+    text: "C#",
+  },
+  {
+    _id: "11",
+    text: "C++",
+  },
+];
+
+export const design = [
+  { _id: "21", img: figma, text: "figma" },
+  { _id: "22", img: adobePS, text: "photoshop" },
+  { _id: "23", img: canva, text: "canva" },
+  { _id: "24", img: adobeAi, text: "illustrator" },
+  { _id: "25", img: adobeAE, text: "afterEffects" },
+  { _id: "26", img: adobeIn, text: "indesign" },
+];
