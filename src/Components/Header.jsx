d3dc088a0334eea686745d3ac737be4defee346a
Sa3fu <saifuden786@gmail.com>
2024-04-02 12:01:40 +0530

    for small screens navbar done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 import logo from "../assets/output-onlinepngtools.png";
+import { motion } from "framer-motion";
 
-const Header = () => {
+const Header = ({ home, about, whatido, skills, experience, works, form }) => {
   const [opennavbar, setOpennavbar] = useState(false);
 
   const handleOpen = () => {
@@ -11,13 +12,19 @@ const Header = () => {
     document.body.style.overflowY = "hidden";
   };
 
-  const handleClose = ({ ref }) => {
+  const handleClose = () => {
+    setOpennavbar(false);
+    document.body.style.overflowY = "auto";
+  };
+
+  const scrollToRef = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" });
     setOpennavbar(false);
     document.body.style.overflowY = "auto";
   };
 
   return (
-    <div className="relative ">
+    <div className="relative">
       <div className="lg:hidden xl:hidden">
         <MenuOutlinedIcon
           className="fixed z-10 top-4 right-6 cursor-pointer focus:hidden"
@@ -26,7 +33,13 @@ const Header = () => {
         />
       </div>
       {opennavbar && (
-        <div className="lg:hidden xl:hidden fixed inset-0 bg-cyan-400 z-50 opacity-95">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 0.95 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          className="lg:hidden xl:hidden fixed inset-0 bg-cyan-600 z-50 opacity-95"
+        >
           <img src={logo} alt="" className="absolute w-14 top-3" />
           <h4 className="font-titleFont flex justify-center items-center text-white top-4 text-xl font-semibold mt-7">
             MOHAMMAD SAIFUDDIN
@@ -38,22 +51,16 @@ const Header = () => {
           />
           <div>
             <ul className="flex flex-col justify-center items-center mt-16 gap-8 text-white text-3xl font-semibold">
-              <li
-                onClick={() =>
-                  ref.home.current.scrollIntoView({ behavior: "smooth" })
-                }
-              >
-                Home
-              </li>
-              <li>About</li>
-              <li>What I Do</li>
-              <li>Skills</li>
-              <li>Experience</li>
-              <li>Portfolio</li>
-              <li>Contact</li>
+              <li onClick={() => scrollToRef(home)}>Home</li>
+              <li onClick={() => scrollToRef(about)}>About</li>
+              <li onClick={() => scrollToRef(whatido)}>What I Do</li>
+              <li onClick={() => scrollToRef(skills)}>Skills</li>
+              <li onClick={() => scrollToRef(experience)}>Experience</li>
+              <li onClick={() => scrollToRef(works)}>Portfolio</li>
+              <li onClick={() => scrollToRef(form)}>Contact</li>
             </ul>
           </div>
-        </div>
+        </motion.div>
       )}
     </div>
   );
